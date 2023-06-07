9bbd9642f53fbe8a8f7e2a4a869719265bc97524
mohatkar-monali <monumohatakr16@gmail.com>
2023-06-07 21:57:52 +0530

    file added

---
+++ C#_Program/temporarycookie/temporarycookie/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="temporarycookie.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Temporary Cookie</p>
+        <p>
+            Name&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Email&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Store in temporary cookie" Width="254px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="View temp cookie" Width="262px" />
+        </p>
+        <p>
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Store in permanant cookie" Width="252px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="View permanant  cookie" Width="249px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ C#_Program/temporarycookie/temporarycookie/WebForm1.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace temporarycookie
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //code to create temporary cookie
+            HttpCookie cookie = new HttpCookie("cook");
+            cookie.Values.Add("name", TextBox1.Text);
+            cookie.Values.Add("email", TextBox2.Text);
+            Response.Cookies.Add(cookie);
+            Label1.Text = "temporary cookie added to client machine";
+       
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //code to  retrieve code from client machine
+            HttpCookie cookie = Request.Cookies["cook"];
+            if(cookie != null)
+            {
+                string name = cookie.Values["name"].ToString();
+                string email = cookie.Values["email"].ToString();
+                Label1.Text = "Name:"+name;
+                Label2.Text = "Email:"+email;
+            }
+            else
+            {
+                Label1.Text = "No cookie added";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            //code to create permanant cookie
+            HttpCookie cookie1 = new HttpCookie("cook1");
+            cookie1.Values.Add("name", TextBox1.Text);
+            cookie1.Values.Add ("email", TextBox2.Text);
+            DateTime dt=DateTime.Now;
+            TimeSpan ts=new TimeSpan(0,1,0);
+            cookie1.Expires = dt.Add(ts);
+            Response.Cookies.Add(cookie1);
+            Label1.Text = "Permanant cookie added";
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            //code to retreive permanant cookie
+            HttpCookie cookie = Request.Cookies["cook1"];
+            if(cookie != null)
+            {
+                string name = cookie.Values["name"].ToString();
+                string email = cookie.Values["email"].ToString();
+                Label1.Text = "Name:" + name;
+                Label2.Text = "Email:" + email;
+            }
+            else
+            {
+                Label1.Text = "No cookie added";
+            }
+        }
+    }
+}

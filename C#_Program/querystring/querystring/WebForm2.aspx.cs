62bccf50c8d555ef17dab0f237bb81d5be8dc059
mohatkar-monali <monumohatakr16@gmail.com>
2023-06-08 10:47:43 +0530

    file added

---
+++ C#_Program/querystring/querystring/WebForm2.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace querystring
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string name,email;
+            name = Request.QueryString["name"].ToString();
+            email = Request.QueryString["email"].ToString();
+            Response.Write("Name:" + name);
+            Response.Write("email:" + email);
+            //Label1.Text = name;
+        }
+    }
+}

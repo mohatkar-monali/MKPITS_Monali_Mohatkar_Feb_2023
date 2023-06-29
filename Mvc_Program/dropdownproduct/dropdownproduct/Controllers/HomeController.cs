75e78cdf0d97b333234163149c8d4965f90eea78
mohatkar-monali <monumohatakr16@gmail.com>
2023-06-29 11:23:17 +0530

    file added

---
+++ Mvc_Program/dropdownproduct/dropdownproduct/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using dropdownproduct.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace dropdownproduct.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Product p)
+        {
+            ViewBag.ono = p.orderno;
+            ViewBag.cname = p.custname;
+            ViewBag.price=p.price;
+            ViewBag.qty = p.quantity;
+            ViewBag.ptype = p.producttype;
+            int tprice = p.price * p.quantity;
+            ViewBag.totalprice=tprice;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mvc_Program/dropdownproduct/dropdownproduct/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace dropdownproduct.Models
+{
+    public class Product
+    {
+        public int orderno { get; set; }
+        public string custname { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }  
+        public product producttype { get; set; }
+        public int totalprice { get; set; }
+    }
+    public enum product
+    {
+        mouse,keyboard,ram,monitor
+    }
+}

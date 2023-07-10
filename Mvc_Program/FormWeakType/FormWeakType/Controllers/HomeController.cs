fdd96ec41bcbbd3b957bebcefac3933cf2dfccf0
mohatkar-monali <monumohatakr16@gmail.com>
2023-07-10 13:12:22 +0530

    file added

---
+++ Mvc_Program/FormWeakType/FormWeakType/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormWeakType.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Form1( int txtId,string txtName,string chkAddon)
+        {
+            ViewBag.Id = txtId;
+            ViewBag.Name=txtName;
+            if (chkAddon != null)
+            {
+                ViewBag.Addon = "Selected";
+            }
+            else
+            {
+                ViewBag.Addon = "Not Selected";
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Mvc_Program/FormWeakType/FormWeakType/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FormWeakType.MvcApplication" Language="C#" %>
